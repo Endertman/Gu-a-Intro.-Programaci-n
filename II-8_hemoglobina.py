2eed1c1850a908e20e67cf6571def6e5e696c80e
Felipe Ochoa <ochoafelipe2004@gmail.com>
2024-03-28 12:59:03 -0300

    final

--- II-8_hemoglobina.py
+++ II-8_hemoglobina.py
@@ -1,9 +1,8 @@
 print("programa para saber los niveles de hemoglobina")
 
-
-meses_float = float(input("ingresa los meses de edad, si tiene mas de 1 año, entonces escriba un numero negativo: "))
-meses = int(meses_float)
-if meses in range(12):
+edad = (int(input('Ingrese  edad, si tiene menos de un año de edad ingrese 0: ')))
+if edad == 0:
+  meses = (int(input('Ingrese  edad en numero de meses')))
   nivel_hemoglobina = float(input("ingresa los niveles de hemoglobina: "))
   if meses < 1:
     if nivel_hemoglobina >=13 and nivel_hemoglobina <=26:
@@ -21,7 +20,6 @@ if meses in range(12):
     else:
       print("el nivel de hemoglobina es negativo, tiene posibilidad de anemia")
 else:
-  edad = int(input("ingresa tu edad: "))
   nivel_hemoglobina = float(input("ingresa los niveles de hemoglobina: "))
   if edad >= 5 and edad <= 10:
     if nivel_hemoglobina >= 12.6 and nivel_hemoglobina <= 15.5:
@@ -34,15 +32,15 @@ else:
     else:
       print("el nivel de hemoglobina es negativo, tiene posibilidad de anemia")
   elif edad >= 15:
-    sexo = input("ingresa tu sexo, masculino o femenino: ")
+    sexo = input("ingresa el sexo, 1. masculino o 2. femenino: ")
     sexo = sexo.upper()
     sexo = sexo.lower()
-    if sexo == "masculino":
+    if sexo == "masculino" or sexo == 1:
       if nivel_hemoglobina >= 14 and nivel_hemoglobina <= 18:
         print("el nivel de hemoglobina es positivo")
       else:
         print("el nivel de hemoglobina es negativo, tiene posibilidad de anemia")
-    elif sexo == "femenino":
+    elif sexo == "femenino" or sexo == 2:
       if nivel_hemoglobina >= 12 and nivel_hemoglobina <= 16:
         print("el nivel de hemoglobina es positivo")
       else:
